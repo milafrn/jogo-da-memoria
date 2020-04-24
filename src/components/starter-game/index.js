3104f740616352439e5f5eca36fa1c504107d210
Mila <kaah.fernandess@hotmail.com>
2020-04-23 22:21:32 -0300

    :construction: Aula 033 - Desafio Intermediário

--- src/components/starter-game/index.js
+++ src/components/starter-game/index.js
@@ -1,7 +1,27 @@
 const starterGame = (function () {
   const module = {};
 
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+      .start {
+        width: 100px;
+        height: 100px;
+        border: none;
+        border-radius: 50%;
+        position: absolute;
+        bottom: 0;
+        left: 35%;
+      }
+    `;
+
+    $head.insertBefore($style, null);
+  };
+
   module.create = () => {
+    module._style();
     return `
       <button class="start">Start</button>
     `;
