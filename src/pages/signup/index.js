4344871078aad5830e59a233fe343c663481e065
Mila <kaah.fernandess@hotmail.com>
2020-05-10 15:34:33 -0300

    :construction: Aula 039 - Desafio Intermediário

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -4,9 +4,11 @@
   const $loginButton = flatButton.render('Log in');
   const $signupButton = flatButton.render('Sign up', true);
   const $logo = logo.render();
+  const $title = title.render('Welcome!');
 
 
   $root.insertAdjacentHTML("beforeend", $loginButton);
   $root.insertAdjacentHTML("beforeend", $signupButton);
   $root.insertAdjacentHTML('beforeend', $logo);
+  $root.insertAdjacentHTML('beforeend', $title);
 })();
